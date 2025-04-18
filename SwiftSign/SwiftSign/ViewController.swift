d505979a4d47a7e33fd40c956d0bb79e2ff6e373
HwangSeokBeom <tjrqja0714@gmail.com>
2025-04-18 22:09:27 +0900

    [Feat]: 프로젝트 초기 설정

---
+++ SwiftSign/SwiftSign/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftSign
+//
+//  Created by Hwangseokbeom on 4/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+
+}
+
---
+++ SwiftSign/SwiftSignTests/SwiftSignTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSignTests.swift
+//  SwiftSignTests
+//
+//  Created by Hwangseokbeom on 4/18/25.
+//
+
+import Testing
+@testable import SwiftSign
+
+struct SwiftSignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
