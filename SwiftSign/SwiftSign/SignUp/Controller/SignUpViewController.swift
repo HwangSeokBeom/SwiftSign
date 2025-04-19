acf1c8145c936776c64b1f3520ba176194a426cd
HwangSeokBeom <tjrqja0714@gmail.com>
2025-04-19 14:59:31 +0900

    [Feat]: 회원가입 컨트롤러 구현

--- SwiftSign/SwiftSign/SignUp/Controller/SignUpViewController.swift
+++ SwiftSign/SwiftSign/SignUp/Controller/SignUpViewController.swift
@@ -5,3 +5,18 @@
 //  Created by Hwangseokbeom on 4/19/25.
 //
 
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    private let signUpView = SignUpView()
+
+    override func loadView() {
+        self.view = signUpView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "회원가입"
+    }
+}
